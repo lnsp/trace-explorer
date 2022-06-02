085706acd4263624951c0f60ff3a8add17e09cde
Lennart Espe <lnsp@users.noreply.github.com>
2022-06-02 20:09:17 +0000

    join: Add concat method

--- trace_explorer/join.py
+++ trace_explorer/join.py
@@ -14,3 +14,11 @@ def join(sources: list[str], output: str):
     for item in items:
         df = df.join(item)
     df.to_parquet(output)
+
+
+def concat(sources: list[str], output: str):
+    dfs = [
+        pd.read_parquet(src)
+        for src in sources
+    ]
+    pd.concat(dfs, ignore_index=True).to_parquet(output)
